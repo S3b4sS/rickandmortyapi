abaacfa5eff06d08dcab8a84d0a00b8be309cab8
ssuf <ssuf2002@gmail.com>
2024-03-23 11:50:01 -0400

    first commit

---
+++ src/components/Card.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { defineProps } from 'vue';
+
+    const props = defineProps({
+        character: []
+    }) 
+</script>
+
+<template>
+    <div>
+        <div>
+            <img src="" alt="desactiva el bloqueador de anuncios para ver las mujeres tetonas">
+
+        </div>
+        <div>
+            <h3>Nombre</h3>
+
+        </div>
+
+    </div>
+
+
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from 'vue';
+import Card from "../components/Card.vue"
+
+let character = ref([])
+
+async function loadCharacters() {
+  const response = await fetch (`https://rickandmortyapi.com/api/character?page=1`);
+  const data = await response.json();
+  character.value = data.results;
+  console.log(data);
+}
+
+loadCharacters()
+</script>
+
+<template>
+  <div class="flex justify-center mt-10">
+    <h1 class="text-5xl">Personajes de Rick y Morty</h1>
+    
+      <button><</button>
+      <button>></button>  
+  </div>
+  <br><br>
+  <div v-for="personaje in character" :key="character.id">{{ personaje.name }}
+
+  </div>
+  
+  <Card/>
+</template>
